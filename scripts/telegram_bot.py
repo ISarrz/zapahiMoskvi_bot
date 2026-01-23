2d4f8bcfbbd3e540bfc42b9b991173b9030dc10f
Zlobin German <gezlobin@edu.hse.ru>
2026-01-23 12:13:37 +0300

    new update 0.4.1

--- scripts/telegram_bot.py
+++ scripts/telegram_bot.py
@@ -14,12 +14,14 @@ from modules.telegram_int.main_menu import ConversationHandler_main_menu
 from telegram.ext import (
     ApplicationBuilder,
     CallbackContext,
-CallbackQueryHandler
+    CallbackQueryHandler,
+    CommandHandler,
 )
 
 from modules.logger.logger import async_logger
 from modules.config.config import get_telegram_message
 from modules.telegram_int.notifications.handlers import notifications_handler
+
 
 @async_logger
 async def send_logs(context: CallbackContext):
@@ -37,11 +39,25 @@ async def send_logs(context: CallbackContext):
 @async_logger
 async def send_reboot_notifications(context: CallbackContext):
     for user in User.all():
-        text = "Проведено техническое обслуживание. Для дальнейшей работы пропишите /start"
-        await context.bot.send_message(chat_id=user.telegram_id, text=text)
+        try:
+            text = "Проведено техническое обслуживание. Для дальнейшей работы пропишите /start"
+            await context.bot.send_message(chat_id=user.telegram_id, text=text)
+        except Exception as e:
+            pass
 
 
+async def send_db(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    user = User(telegram_id=update.effective_user.id)
+    if user.role != "admin":
+        return
 
+    with open(database_path, "rb") as db_file:
+        await context.bot.send_document(
+            chat_id=chat_id,
+            document=db_file,
+            filename="database.db"
+        )
 
 
 @async_logger
@@ -63,14 +79,14 @@ def main():
 
     application.add_handler(ConversationHandler_main_menu, 1)
     application.add_handler(CallbackQueryHandler(notifications_handler, pattern="настройка напоминаний"))
-
+    application.add_handler(CommandHandler("get_db", send_db))
     job_deque = application.job_queue
     job_deque.run_repeating(send_logs, 20)
     job_deque.run_once(send_reboot_notifications, 0)
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
-from modules.config.paths import telegram_messages_path
+from modules.config.paths import telegram_messages_path, database_path
 
 print(telegram_messages_path)
 if __name__ == '__main__':
